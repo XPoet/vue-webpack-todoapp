40c2cac18b119eac109549eac6b8714a0076f2f1
itPoet <i@itpoet.cn>
2018-03-12 14:57:16 +0800

    README文件整理

--- src/todo/footer.jsx
+++ src/todo/footer.jsx
@@ -3,13 +3,14 @@ import '../assets/styles/footer.styl'
 export default {
     data() {
         return {
-            author: 'itPoet'
+            author: 'itPoet',
+            blog: 'itPoet.cn'
         }
     },
     render() {
         return (
             <div id="footer">
-                <span>Power by {this.author}</span>
+                <span>Power by {this.author}，欢迎访问作者博客：{this.blog}</span>
             </div>
         )
     }
